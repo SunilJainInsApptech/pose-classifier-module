135a2a0d8111f69005a34f20be9af5a0d13d61e1
Sunil Jain <sjain@apptech.insure>
2025-08-18 16:44:59 -0400

    debug 27

--- pose-classifier/src/download.py
+++ pose-classifier/src/download.py
@@ -1,6 +1,9 @@
 import torch
 from ultralytics import YOLO
 
-model = YOLO('yolo11n-pose.pt')  # or your correct .pt file
-torch.save(model.state_dict(), 'model.pt')
-torch.load('model.pt') 
+model = YOLO('/home/sunil/pose-classifier-module/pose-classifier/src/yolo11n-pose.pt')  # or your correct .pt file
+scripted_model = torch.jit.script(model)
+scripted_model.save('/home/sunil/pose-classifier-module/pose-classifier/src/model.pt')
+
+model = torch.jit.load('model.pt')
+print(type(model))
