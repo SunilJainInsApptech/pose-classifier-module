571de7b69c4eb202f53b14acd579e4f11afb2a8d
Sunil Jain <sjain@apptech.insure>
2025-09-01 22:56:07 -0400

    debug 95

--- fall_detection_alerts.py
+++ fall_detection_alerts.py
@@ -42,7 +42,13 @@ class FallDetectionAlerts:
         if env_phones:
             self.to_phones = [phone.strip() for phone in env_phones.split(',')]
         else:
-            self.to_phones = config.get('twilio_to_phones', '+19738652226')
+            config_phones = config.get('twilio_to_phones', '+19738652226')
+            if isinstance(config_phones, str):
+                self.to_phones = [phone.strip() for phone in config_phones.split(',')]
+            elif isinstance(config_phones, list):
+                self.to_phones = [str(phone).strip() for phone in config_phones]
+            else:
+                self.to_phones = ['+19738652226']
         
         self.webhook_url = (
             os.environ.get('TWILIO_WEBHOOK_URL') or 
