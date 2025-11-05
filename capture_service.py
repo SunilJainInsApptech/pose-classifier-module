ed55ff3b838c7bc757d0cb82992038e7448cc0d2
Sunil Jain <sjain@apptech.insure>
2025-11-05 16:01:02 -0500

    debug 060

--- capture_service.py
+++ capture_service.py
@@ -16,9 +16,9 @@ LOGGER = logging.getLogger(__name__)
 
 # --- Configuration (Should match your main script) ---
 RTSP_STREAMS = {
-    'Lobby_Center_North': 'rtsp://70.19.68.121:554/chID=25&streamType=sub',
-    'CPW_Awning_N_Facing': 'rtsp://70.19.68.121:554/chID=16&streamType=sub',
-    'Roof_Front_East_Facing': 'rtsp://70.19.68.121:554/chID=01&streamType=sub?tcp',
+    'Lobby_Center_North': 'rtsp://70.19.68.121:554/chID=25',
+    'CPW_Awning_N_Facing': 'rtsp://70.19.68.121:554/chID=16',
+    'Roof_Front_East_Facing': 'rtsp://70.19.68.121:554/chID=01',
     # Add other stream names and URLs here
 }
 
